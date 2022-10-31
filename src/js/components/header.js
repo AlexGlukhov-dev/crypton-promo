8da73d7570cb4751371e00b46426a904e496bcac
Vladinero <vlad.servicemail@gmail.com>
2022-10-31 16:14:18 +0300

    Init commit

--- src/js/components/header.js
+++ src/js/components/header.js
@@ -1,34 +1,15 @@
 const progress = document.getElementById("progressbar");
-const rotated = document.getElementById("menu-button");
 const upBtn = document.querySelector(".btn-up");
 
 let totalHeight = document.body.scrollHeight - window.innerHeight;
-// progress.style.height = '0';
 
 window.addEventListener("scroll", () => {
-  let progressHeight = (window.scrollY / totalHeight) * 74.32;
+  let progressHeight = (window.scrollY / totalHeight) * 100;
   progress.style.height = progressHeight + "%";
 });
 
-const parallax = (e) => {
-document.querySelectorAll('.layer')
-  .forEach(layer => {
-    const speed = layer.getAttribute('data-speed')
-    const x = (window.innerWidth - e.pageX * speed) / 50
-    const y = (window.innerHeight - e.pageY * speed) / 50
-    layer.style.transform = `translateX(${x}px) translateY(${y}px)`
-  })
-}
-
-// if (window.matchMedia(("(min-width: 768px")).matches) {
-//   window.addEventListener("mousemove", parallax);
-// }
-window.addEventListener("mousemove", parallax);
-
 window.addEventListener("scroll", () => {
-  let offset = window.scrollY * 0.2;
 
-  rotated.style.transform = `rotate(${offset}deg)`;
 
   if (window.scrollY > 100) {
     upBtn.classList.remove("visually-hidden")

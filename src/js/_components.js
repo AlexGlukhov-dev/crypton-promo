d33e055cf2544f555fb8d1964629dc6e2d6831b1
Vladinero <vlad.servicemail@gmail.com>
2022-10-31 16:14:18 +0300

    Init commit

--- src/js/_components.js
+++ src/js/_components.js
@@ -1,7 +1,3 @@
-import "./components/mobile-menu";
+// import "./components/mobile-menu";
 import "./components/header";
-import "./components/about";
-import "./components/directions";
-import "./components/events";
-import "./components/contact";
-import "./components/footer";
+// import "./components/footer";
